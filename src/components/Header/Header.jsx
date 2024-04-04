6f246b4770c0227287ef7f91749ff254000fbf91
Leonardo Martin <leonardomartin.dev@gmail.com>
2024-04-04 19:06:07 -0300

    download btn adjust text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
                 <DeveloperTitle>&lt;Developer/&gt;</DeveloperTitle>
             </div>
             <BtnContainer>
-                <DownloadCv> <a href={resume} download>Downlaod CV</a></DownloadCv>
+                <DownloadCv> <a href={resume} download>Download CV</a></DownloadCv>
                 <ContactBtn> <a href="https://api.whatsapp.com/send/?phone=551915630912&text&type=phone_number&app_absent=0" target="blank">Contact</a></ContactBtn>
             </BtnContainer>
         </InfoContainer>
